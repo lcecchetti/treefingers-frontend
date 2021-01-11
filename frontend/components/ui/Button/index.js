20e61872f794a3beaddcd959a0e189f500f7520e
Luca Cecchetti <luca.cecchetti07@gmail.com>
2021-01-11 01:44:53 +0100

    add link ui component

--- frontend/components/ui/Button/index.js
+++ frontend/components/ui/Button/index.js
@@ -7,12 +7,12 @@ const Button = ({ children, className, loading, variant, as, Icon, ...rest }) =>
 
   return (
     <Component
-      class={clsx(
-        'rounded-full py-2 px-4 inline-flex items-center transition-colors focus:outline-none',
+      className={clsx(
+        'rounded-full no-underline py-2 px-4 inline-flex items-center transition-colors focus:outline-none',
         {
-          ['bg-primary text-primary-contrast active:bg-primary-dark disabled:bg-primary-light']: !variant,
-          ['bg-secondary text-secondary-constrast active:bg-secondary-dark disabled:bg-secondary-light']: variant === 'secondary',
-          ['bg-primary-contrast text-primary border-primary border-2 disabled:border-primary-light disabled:text-primary-light active:bg-primary active:text-primary-contrast']: variant === 'outlined',
+          ['bg-primary text-primary-contrast hover:text-primary-contrast active:bg-primary-dark disabled:bg-primary-light']: !variant,
+          ['bg-secondary text-secondary-constrast hover:text-secondary-constrast active:bg-secondary-dark disabled:bg-secondary-light']: variant === 'secondary',
+          ['bg-primary-contrast text-primary hover:text-primary border-primary border-2 disabled:border-primary-light disabled:text-primary-light active:bg-primary active:text-primary-contrast']: variant === 'outlined',
         },
         className
       )}
