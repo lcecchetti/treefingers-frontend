99e451d3774842f59d0594798bee381541ec2396
Luca Cecchetti <luca.cecchetti07@gmail.com>
2021-01-31 20:26:56 -0400

    init main navigation

--- frontend/components/common/Header/index.js
+++ frontend/components/common/Header/index.js
@@ -1,6 +1,7 @@
-import { Container, Link, Button } from 'components/ui';
+import { Container, Link, Text } from 'components/ui';
 import { Logo } from 'components/common';
 import { gql, useQuery } from '@apollo/client';
+import { FaSearch, FaPenFancy, FaUserCircle } from 'react-icons/fa';
 
 /**
  * Self query
@@ -23,12 +24,29 @@ const Header = () => {
     <div className="absolute w-full h-header bg-base">
       <Container>
         <div className="flex items-center justify-between my-md">
-          <Logo main className="bold" />
-          <ul className="flex flex-row gap-sm">
+          <Logo main className="font-bold" />
+
+          <ul className="hidden sm:flex flex-row gap-xl">
             <li>
-              <Button as={Link} href={data ? '/profile/me' : '/auth/login'}>
-                {data ? `Welcome, ${data.self?.username}!` : 'Login'}
-              </Button>
+              <Link href="/stories" underline={false}><Text variant="span" className="font-bold text-lg">ALL STORIES</Text></Link>
+            </li>
+            <li>
+              <Link href="/stories/pop" underline={false}><Text variant="span" className="font-bold text-lg">POP</Text></Link>
+            </li>
+            <li>
+              <Link href="/stories/new" underline={false}><Text variant="span" className="font-bold text-lg">NEW</Text></Link>
+            </li>
+          </ul>
+
+          <ul className="flex flex-row gap-sm sm:gap-md">
+            <li>
+              <Link href="/story/search"><FaSearch className="text-xl md:text-2xl"/></Link>
+            </li>
+            <li>
+              <Link href="/story/create"><FaPenFancy className="text-xl md:text-2xl"/></Link>
+            </li>
+            <li>
+              <Link href={data ? '/profile/me' : '/auth/login'}><FaUserCircle className="text-xl md:text-2xl"/></Link>
             </li>
           </ul>
         </div>
