a8296af7314e472d8b3ecc541c4179f0eea6c548
Luca Cecchetti <luca.cecchetti07@gmail.com>
2021-02-04 21:02:26 -0400

    preload story serverside

--- frontend/pages/story/[slug].js
+++ frontend/pages/story/[slug].js
@@ -1,7 +1,7 @@
 import { DefaultLayout } from 'components/layout';
 import { Text, Container } from 'components/ui';
 import { initializeApollo, addApolloState } from 'lib/apollo/client';
-import { StoryView, QUERY_STORIES, QUERY_STORY_BY_SLUG } from 'components/story';
+import { StoryView, QUERY_STORIES, QUERY_STORY_BY_SLUG, QUERY_STORY } from 'components/story';
 import { getStoryUrl } from 'lib/helper/story';
 
 const StoryPage = ({ story }) => {
@@ -16,14 +16,22 @@ const StoryPage = ({ story }) => {
 export async function getStaticProps({ params }) {
   const apolloClient = initializeApollo();
 
+  // load story by slug
   const { data } = await apolloClient.query({
     query: QUERY_STORY_BY_SLUG,
     variables: { slug: params.slug },
+  },);
+
+  // add story by id query to the cache
+  apolloClient.writeQuery({
+    query: QUERY_STORY,
+    data: { story: data.storyBySlug },
+    variables: { id: data.storyBySlug.id },
   });
 
   return addApolloState(apolloClient, {
     props: { story: data.storyBySlug},
-    //revalidate: 1,
+    revalidate: 1,
   });
 }
 
