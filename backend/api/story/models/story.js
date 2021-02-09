f89e34b793fca876e9e9233ce9342dba04e1b7f8
Luca Cecchetti <luca.cecchetti07@gmail.com>
2021-02-09 16:53:58 -0400

    update slug system to use strapi uuid

--- backend/api/story/models/story.js
+++ backend/api/story/models/story.js
@@ -5,21 +5,4 @@
  * to customize this model
  */
 
-const slugify = require('slugify');
-
-module.exports = {
-  lifecycles: {
-    beforeCreate: async (data) => {
-      //@todo check for existing same slug and append an incremental number in the end in case
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
-      }
-    },
-    beforeUpdate: async (params, data) => {
-      //@todo remove when ready to make slug permanent even on title update
-      if (data.title) {
-        data.slug = slugify(data.title, { lower: true });
-      }
-    },
-  },
-};
+module.exports = {};
