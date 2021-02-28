3a8dcea45fb9a0ac08874e30578bba70b3d86c5e
Luca Cecchetti <luca.cecchetti07@gmail.com>
2021-02-27 20:13:23 -0400

    fix logout

--- frontend/lib/auth/token.js
+++ frontend/lib/auth/token.js
@@ -2,6 +2,8 @@ import { Cookies } from 'react-cookie';
 
 const cookies = new Cookies();
 
+const authCookieOptions = { path: '/' };
+
 /**
  * Get auth token
  * @return {string}
@@ -15,14 +17,14 @@ const getAuthToken = () => {
  * @param {string} authToken 
  */
 const setAuthToken = (authToken) => {
-  cookies.set(process.env.NEXT_PUBLIC_AUTH_TOKEN, authToken);
+  cookies.set(process.env.NEXT_PUBLIC_AUTH_TOKEN, authToken, authCookieOptions);
 }
 
 /**
  * Remove auth token
  */
 const removeAuthToken = () => {
-  cookies.remove(process.env.NEXT_PUBLIC_AUTH_TOKEN);
+  cookies.remove(process.env.NEXT_PUBLIC_AUTH_TOKEN, authCookieOptions);
 }
 
 export { setAuthToken, getAuthToken, removeAuthToken };
