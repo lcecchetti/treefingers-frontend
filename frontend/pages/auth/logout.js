0f0481a7b81eb5282069533311891fa959eb523f
Luca Cecchetti <luca.cecchetti07@gmail.com>
2021-02-28 12:38:25 -0400

    adjust authenticating and logging out style

--- frontend/pages/auth/logout.js
+++ frontend/pages/auth/logout.js
@@ -1,9 +1,31 @@
+import { useEffect } from 'react';
 import { DefaultLayout } from 'components/layout';
-import { Logout } from 'components/auth';
+import { useRouter } from 'next/router';
+import { useApolloClient } from '@apollo/client';
+import { getAuthToken, removeAuthToken } from 'lib/auth';
+import { Spinner, Text, Container } from 'components/ui';
 
 const LogoutPage = () => {
+  const router = useRouter()
+  const client = useApolloClient();
+
+  const logout = async () => {
+    removeAuthToken();
+
+    await client.resetStore();
+
+    router.push('/');
+  };
+
+  useEffect(() => {
+    logout();
+  }, [getAuthToken]);
+
   return (
-    <Logout/>
+    <Container className="flex gap-sm items-center justify-center">
+      <Text variant="span">Logging out...</Text>
+      <Spinner />
+    </Container>
   );
 };
 
