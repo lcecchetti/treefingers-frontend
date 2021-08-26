a32f178bafb2f5821849cc46a3d76defc11ecb6c
Luca Cecchetti <luca.cecchetti07@gmail.com>
2021-08-26 18:27:54 +0200

    move backend code to its own folder

--- backend/lib/apollo/typeDefs.js
+++ backend/lib/apollo/typeDefs.js
@@ -1,10 +1,10 @@
 import { gql } from 'apollo-server-micro';
-import paginationTypeDefs from 'lib/api/pagination/typeDefs';
-import storyTypeDefs from 'lib/api/story/typeDefs';
-import userTypeDefs from 'lib/api/user/typeDefs';
-import commentTypeDefs from 'lib/api/comment/typeDefs';
-import likeTypeDefs from 'lib/api/like/typeDefs';
-import tagTypeDefs from 'lib/api/tag/typeDefs';
+import paginationTypeDefs from 'backend/api/pagination/typeDefs';
+import storyTypeDefs from 'backend/api/story/typeDefs';
+import userTypeDefs from 'backend/api/user/typeDefs';
+import commentTypeDefs from 'backend/api/comment/typeDefs';
+import likeTypeDefs from 'backend/api/like/typeDefs';
+import tagTypeDefs from 'backend/api/tag/typeDefs';
 
 const customScalarsTypeDefs = gql`
   scalar Date
---
+++ backend/lib/mongodb/models.js
@@ -0,0 +1,13 @@
+import User from 'backend/api/user/model';
+import Story from 'backend/api/story/model';
+import Comment from 'backend/api/comment/model';
+import Like from 'backend/api/like/model';
+import Tag from 'backend/api/tag/model';
+
+export {
+  User,
+  Story,
+  Comment,
+  Like,
+  Tag,
+};
