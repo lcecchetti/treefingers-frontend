051391c1bbd00addd865370fee3d00df558a418a
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-01-26 22:03:34 +0100

    remove popular author component

--- pages/stories.js
+++ pages/stories.js
@@ -4,7 +4,6 @@ import { StoryList } from 'components/story';
 import { QUERY_STORIES } from 'components/story/StoryList';
 import { initializeApollo, addApolloState } from 'lib/apollo/client';
 import { PageIntro } from 'components/common';
-import { QUERY_AUTHORS_POPULAR } from 'components/author/PopularAuthors';
 
 const StoriesPage = () => {
   return (
@@ -25,10 +24,6 @@ export async function getStaticProps() {
   await apolloClient.query({
     query: QUERY_STORIES,
     variables: { filter: { root: null } },
-  });
-
-  await apolloClient.query({
-    query: QUERY_AUTHORS_POPULAR,
   });
 
   return addApolloState(apolloClient, {
