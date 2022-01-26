0252f2a80594dbcf771d7910d8a58c5fb3fa0b67
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-01-26 22:03:34 +0100

    remove popular author component

--- components/author/index.js
+++ components/author/index.js
@@ -1,4 +1,3 @@
 export { default as AuthorList } from './AuthorList';
 export { default as AuthorView } from './AuthorView';
 export { default as AuthorCard } from './AuthorCard';
-export { default as PopularAuthors } from './PopularAuthors';
