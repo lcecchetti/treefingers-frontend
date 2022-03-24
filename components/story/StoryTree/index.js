beb58114f1a8a146d36c58d793e597a09d983957
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-03-25 00:14:48 +0100

    tweak tree params

--- components/story/StoryTree/index.js
+++ components/story/StoryTree/index.js
@@ -3,37 +3,39 @@ import clsx from 'clsx';
 
 const TreeView = function(canvas) {
 	this.canvas = canvas;
-	this.ratio = window.devicePixelRatio >= 1 ? window.devicePixelRatio : 1;
 	this.context = this.canvas.getContext( '2d' );
 	
 	this.numberOfChildNodes = 3;
-	this.childLength = 0.9;
+	this.childLengthModifier = 0.9;
 	this.baseTheta = Math.PI * (80 / 180);
 	this.nodeLevels = 7;
-	this.lineWidth = 8;
+	this.lineWidth = 15;
+	this.hue = Math.random() * 360;
 	
 	this.addEventListeners();
-	this.hue = Math.random() * 360;
-
   this.resizeCanvas();
+	this.render();
 };
 		
 TreeView.prototype = {
 	addEventListeners : function() {
-    window.onresize = this.resizeCanvas.bind(this);
+    window.onresize = (function() {
+			this.resizeCanvas();
+			this.render();
+		}).bind(this);
 	},
 	
 	resizeCanvas : function(e) {
-		this.canvas.width = this.canvas.offsetWidth * this.ratio;
-		this.canvas.height = this.canvas.offsetHeight * this.ratio;
+		const devicePixelRatio = window.devicePixelRatio >= 1 ? window.devicePixelRatio : 1;
+
+		this.canvas.width = this.canvas.offsetWidth * devicePixelRatio;
+		this.canvas.height = this.canvas.offsetHeight * devicePixelRatio;
 		this.width = this.canvas.width;
 		this.height = this.canvas.height;
-
-    this.render();
 	},
 			
 	loop : function() {
-		requestAnimationFrame( this.loop.bind(this) );
+		requestAnimationFrame(this.loop.bind(this));
 		this.render();
 	},
 	
@@ -57,39 +59,38 @@ TreeView.prototype = {
 	  return Math.random() * (max - min) + min;
 	},
 	
-	generateLine : function( prevLineNode, prevLevel, totalLevels ) {
-		const lineNode = new LineNode();
+	generateLine : function(prevLineNode, prevLevel) {
 		const currentLevel = prevLevel - 1;
-		const ratioTop = (totalLevels - currentLevel) / totalLevels;
+		const ratioTop = (this.nodeLevels - currentLevel) / this.nodeLevels;
 		const randomness = 2 * (Math.random() - 0.5);
 		const thetaChange = this.baseTheta * randomness * ratioTop;
 		const theta = prevLineNode.theta - thetaChange; //Theta is the previous angle, minus base theta
-		const hyp = prevLineNode.distance * this.childLength;
+		const hyp = prevLineNode.distance * this.childLengthModifier;
 		
-		lineNode.beg.copy(prevLineNode.end);
-		lineNode.end.x = prevLineNode.end.x + ( hyp ) * Math.cos( theta );
-		lineNode.end.y = prevLineNode.end.y + ( hyp ) * Math.sin( theta );
-		
+		const lineNode = new LineNode();
+		lineNode.from.copy(prevLineNode.to);
+		lineNode.to.x = prevLineNode.to.x + hyp * Math.cos(theta);
+		lineNode.to.y = prevLineNode.to.y + hyp * Math.sin(theta);
 		lineNode.update();
 		
-		prevLineNode.children.push( lineNode );
+		prevLineNode.children.push(lineNode);
 				
 		if(currentLevel > 0) {
 			for(const i=0; i < this.numberOfChildNodes; i++) {
-				this.generateLine( lineNode, currentLevel, totalLevels );
+				this.generateLine(lineNode, currentLevel);
 			}
 		}
 	},
 	
-	renderTree : function( lineNode, prevLevel, totalLevels ) {
-		const ratio = prevLevel / totalLevels;
-		const ratio2 = ( (ratio * ratio) + ratio ) / 2;
+	renderTree : function(lineNode, prevLevel) {
+		const ratio = prevLevel / this.nodeLevels;
+		const ratio2 = ((ratio * ratio) + ratio) / 2;
 		
 		this.context.lineWidth = ratio2 * this.lineWidth;
 		
 		this.context.beginPath();
-		this.context.moveTo( lineNode.beg.x, lineNode.beg.y );
-		this.context.lineTo( lineNode.end.x, lineNode.end.y );
+		this.context.moveTo(lineNode.from.x, lineNode.from.y);
+		this.context.lineTo(lineNode.to.x, lineNode.to.y);
 		this.context.strokeStyle = this.hslToFillStyle(
 			this.hue - 90 * ratio,
 			30 * (1 - ratio2) + 10,
@@ -100,7 +101,7 @@ TreeView.prototype = {
 		this.context.closePath();
 		
 	   	for(const i=0; i < lineNode.children.length; i++) {
-	   		this.renderTree( lineNode.children[i], prevLevel - 1, totalLevels );
+	   		this.renderTree(lineNode.children[i], prevLevel - 1);
 	   	}
 	},
 	
@@ -108,13 +109,13 @@ TreeView.prototype = {
     this.reset();
 		
 		const lineNode = new LineNode();	
-    lineNode.beg.x = this.width / 2;
-		lineNode.beg.y = this.height;
-		lineNode.end.x = this.width / 2;
-		lineNode.end.y = this.height - (this.height / 6);
+    lineNode.from.x = this.width / 2;
+		lineNode.from.y = this.height;
+		lineNode.to.x = this.width / 2;
+		lineNode.to.y = this.height - (this.height / 6);
 		lineNode.update();
 		
-		this.generateLine(lineNode, this.nodeLevels, this.nodeLevels);
+		this.generateLine(lineNode, this.nodeLevels);
 		
 		this.context.strokeStyle = this.hslToFillStyle(180, 50, 50);
 		this.context.lineCap = 'round';
@@ -129,9 +130,8 @@ TreeView.prototype = {
 };
 
 const LineNode = function() {
-	this.beg = new Point();
-	this.end = new Point();
-	this.segment = new Point();
+	this.from = new Point();
+	this.to = new Point();
 	this.distance = undefined;
 	
 	this.children = [];
@@ -139,11 +139,11 @@ const LineNode = function() {
 
 LineNode.prototype = {
 	update : function() {
-		this.segment.x = this.end.x - this.beg.x;
-		this.segment.y = this.end.y - this.beg.y;
+		const distanceX = this.to.x - this.from.x;
+		const distanceY = this.to.y - this.from.y;
 		
-		this.distance = Math.sqrt( this.segment.x * this.segment.x + this.segment.y * this.segment.y );
-		this.theta = Math.atan2( this.segment.y, this.segment.x );
+		this.distance = Math.sqrt(distanceX ** 2 + distanceY ** 2);
+		this.theta = Math.atan2(distanceY, distanceX);
 	}
 };
 
@@ -155,7 +155,7 @@ const Point = function (x, y) {
 Point.prototype = {
 	constructor: Point,
 
-	copy: function ( v ) {
+	copy: function (v) {
 		this.x = v.x;
 		this.y = v.y;
 		return this;
