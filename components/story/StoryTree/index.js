ba059d04f5d9bc4adf488d22f69ca71d53bddd4d
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-03-25 18:30:51 +0100

    refactor tree view as classes

--- components/story/StoryTree/index.js
+++ components/story/StoryTree/index.js
@@ -1,181 +1,164 @@
 import { useRef, useEffect } from 'react';
 import clsx from 'clsx';
 
-const TreeView = function(canvas) {
-	this.canvas = canvas;
-	this.context = this.canvas.getContext( '2d' );
-	
-	this.root = undefined;
+class TreeView {
+	constructor(canvas) {
+		this.canvas = canvas;
+		this.context = this.canvas.getContext('2d');
 
-	this.numberOfChildNodes = 3;
-	this.childLengthModifier = 0.9;
-	this.baseTheta = Math.PI * (80 / 180);
-	this.nodeLevels = 7;
-	this.lineWidth = 15;
-	this.hue = Math.random() * 360;
-	
-	this.addEventListeners();
-  this.resizeCanvas();
-	
-	this.update();
-	this.render();
-};
-		
-TreeView.prototype = {
-	addEventListeners : function() {
-    window.onresize = (function() {
+		this.root = undefined;
+
+		this.childrenCount = 3;
+		this.childLengthModifier = 0.9;
+		this.baseTheta = Math.PI * (80 / 180);
+		this.levels = 8;
+		this.baseLineWidth = 20;
+		this.hue = this.random(0, 360);
+
+		this.addEventListeners();
+		this.resizeCanvas();
+
+		this.update();
+		this.render();
+	}
+
+	addEventListeners() {
+		window.onresize = (function () {
 			this.resizeCanvas();
 			this.update();
 			this.render();
 		}).bind(this);
-	},
-	
-	resizeCanvas : function(e) {
+	}
+
+	resizeCanvas() {
 		const devicePixelRatio = window.devicePixelRatio >= 1 ? window.devicePixelRatio : 1;
 
 		this.canvas.width = this.canvas.offsetWidth * devicePixelRatio;
 		this.canvas.height = this.canvas.offsetHeight * devicePixelRatio;
 		this.width = this.canvas.width;
 		this.height = this.canvas.height;
-	},
-			
-	loop : function() {
+	}
+
+	loop() {
 		requestAnimationFrame(this.loop.bind(this));
+		this.update();
 		this.render();
-	},
-	
-	rgbToFillStyle : function(r, g, b, a) {
-		if(a === undefined) {
+	}
+
+	rgbToFillStyle(r, g, b, a) {
+		if (a === undefined) {
 			return `rgb(${r},${g},${b})`;
 		} else {
 			return `rgb(${r},${g},${b},${a})`;
 		}
-	},
-	
-	hslToFillStyle : function(h, s, l, a) {
-		if(a === undefined) {
+	}
+
+	hslToFillStyle(h, s, l, a) {
+		if (a === undefined) {
 			return `hsl(${h},${s}%,${l}%)`;
 		} else {
 			return `hsl(${h},${s}%,${l}%,${a})`;
 		}
-	},
-	
-	random : function(min, max) {
-	  return Math.random() * (max - min) + min;
-	},
-	
-	update: function() {
-		this.root = new Branch();	
-    this.root.from.x = this.width / 2;
-		this.root.from.y = this.height;
-		this.root.to.x = this.width / 2;
-		this.root.to.y = this.height - (this.height / 6);
-		this.root.update();
-		
+	}
+
+	random(min, max) {
+		return Math.random() * (max - min) + min;
+	}
+
+	update() {
+		this.root = new Branch(
+			new Node(this.width / 2, this.height),
+			new Node(this.width / 2, this.height - (this.height / 6))
+		);
+
 		this.generateBranch(this.root);
-	},
+	}
 
-	generateBranch : function(parent) {
-		const ratioTop = parent.level / this.nodeLevels;
-		const randomness = 2 * (Math.random() - 0.5);
-		const thetaChange = this.baseTheta * randomness * ratioTop;
-		const theta = parent.theta - thetaChange; //Theta is the previous angle, minus base theta
-		const hyp = parent.length * this.childLengthModifier;
+	generateBranch(parent) {
+		const ratio = parent.level / this.levels;
+		const thetaChange = this.baseTheta * ratio * this.random(-1, 1);
+		const theta = parent.theta - thetaChange;
+		const hyp = parent.length * this.childLengthModifier * this.random(.9, 1);
 
-		const branch = new Branch();
-		branch.from.copy(parent.to);
-		branch.to.x = parent.to.x + hyp * Math.cos(theta);
-		branch.to.y = parent.to.y + hyp * Math.sin(theta);
-		branch.level = parent.level + 1;
-		branch.update();
-		
+		const branch = new Branch(
+			parent.to,
+			new Node(
+				parent.to.x + hyp * Math.cos(theta),
+				parent.to.y + hyp * Math.sin(theta)
+			),
+			parent.level + 1
+		);
+
 		parent.children.push(branch);
-				
-		if(branch.level < this.nodeLevels) {
-			for(const i = 0; i < this.numberOfChildNodes; i++) {
+
+		if (branch.level < this.levels) {
+			for (const i = 0; i < this.childrenCount; i++) {
 				this.generateBranch(branch);
 			}
 		}
-	},
-	
-	renderTree : function(branch) {
-		this.context.strokeStyle = this.hslToFillStyle(180, 50, 50);
-		this.context.lineCap = 'round';
+	}
 
-		const ratio =  (this.nodeLevels - branch.level) / this.nodeLevels;
-		const ratio2 = ((ratio ** 2) + ratio) / 2;
-		
-		this.context.lineWidth = ratio2 * this.lineWidth;
-		
+	renderTree(branch) {
+		const ratio = (this.levels - branch.level) / this.levels;
+
+		this.context.lineCap = 'round';
+		this.context.lineWidth = ratio * this.baseLineWidth;
+
 		this.context.beginPath();
 		this.context.moveTo(branch.from.x, branch.from.y);
 		this.context.lineTo(branch.to.x, branch.to.y);
 		this.context.strokeStyle = this.hslToFillStyle(
 			this.hue - 90 * ratio,
-			30 * (1 - ratio2) + 10,
-			(30 * (1 - ratio) + 30) * this.random(0.8, 1),
-			0.9
+			30 * (1 - ratio) + 10,
+			(30 * (1 - ratio) + 30) * this.random(.8, 1),
+			.9
 		);
 		this.context.stroke();
 		this.context.closePath();
-		
-	   	for(const i = 0; i < branch.children.length; i++) {
-	   		this.renderTree(branch.children[i]);
-	   	}
-	},
-	
-	render : function() {		
-    this.reset();
+
+		for (const i = 0; i < branch.children.length; i++) {
+			this.renderTree(branch.children[i]);
+		}
+	}
+
+	render() {
+		this.reset();
 		this.renderTree(this.root);
-	},
+	}
 
-  reset : function() {
-		this.context.fillStyle = this.rgbToFillStyle(255, 255, 255, 0);
-		this.context.fillRect(0,0,this.width, this.height);
-	},
-};
+	reset() {
+		this.context.clearRect(0, 0, this.width, this.height);
+	}
+}
+		
+class Branch {
+	constructor(from, to, level = 1) {
+		this.from = from;
+		this.to = to;
+		this.level = level;
+		this.children = [];
 
-const Branch = function() {
-	this.from = new Node();
-	this.to = new Node();
-	this.length = undefined;
-	this.theta = undefined;
-	this.level = 1;
-	
-	this.children = [];
-};
-
-Branch.prototype = {
-	update : function() {
 		const lengthX = this.to.x - this.from.x;
 		const lengthY = this.to.y - this.from.y;
-		
+
 		this.length = Math.sqrt(lengthX ** 2 + lengthY ** 2);
 		this.theta = Math.atan2(lengthY, lengthX);
 	}
-};
+}
 
-const Node = function (x, y) {
-	this.x = x || 0;
-	this.y = y || 0;
-};
-
-Node.prototype = {
-	constructor: Node,
-
-	copy: function (v) {
-		this.x = v.x;
-		this.y = v.y;
-		return this;
-	},
-};
+class Node {
+	constructor(x, y) {
+		this.x = x || 0;
+		this.y = y || 0;
+	}
+}
 
 const StoryTree = ({ story, className }) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     new TreeView(canvasRef.current);
-  }, [])
+  }, [story])
 
   return (
     <div className={clsx('', className)}>
