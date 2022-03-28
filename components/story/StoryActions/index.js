b8ce1e186d66f688c2687e4730c163eed06578ee
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-03-28 23:19:32 +0200

    add tabs for new or pick a story

--- components/story/StoryActions/index.js
+++ components/story/StoryActions/index.js
@@ -1,13 +1,14 @@
+import clsx from 'clsx';
 import { CommentCount } from 'components/comment';
 import { Like } from 'components/common';
 import { useUI, flyoutTypes } from 'lib/ui/context';
 
-const StoryActions = ({ story }) => {
+const StoryActions = ({ story, className }) => {
 
   const { openFlyout } = useUI();
 
   return (
-    <div className="flex items-center gap-sm">
+    <div className={clsx('flex items-center gap-sm', className)}>
       <CommentCount count={story.commentsCount} action={() => openFlyout(flyoutTypes.comments, { entity: story, title: 'Comments' })} />
       <Like entity={story} />
     </div>
