f9f2097062d4235da87b0c8dc6883ec1809ba690
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-04-02 18:08:20 +0200

    change font families

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ class CustomDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&family=Open+Sans:wght@400;700&display=swapswap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
