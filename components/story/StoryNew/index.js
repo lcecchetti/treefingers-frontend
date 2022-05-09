f5db18448ea943c4e2de0d2f26287d578ba1fff9
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-05-09 16:38:55 +0200

    improve forest search on story submission

--- components/story/StoryNew/index.js
+++ components/story/StoryNew/index.js
@@ -1,6 +1,6 @@
 import { gql, useApolloClient, useMutation, useQuery } from '@apollo/client';
 import { Formik, Form, Field, FieldArray } from 'formik';
-import { FormField, Button, Text, Spinner } from 'components/ui';
+import { FormField, Button, Text } from 'components/ui';
 import * as Yup from 'yup';
 import { getStoryUrl } from 'lib/helper/story';
 import { AuthRequired } from 'components/auth';
@@ -54,152 +54,160 @@ const StoryNew = ({ parent, forest, className }) => {
     },
   });
 
-  const { data: forestsData, loading: loadingForests } = useQuery(QUERY_CHOOSE_FOREST, {
+  const hasForestSelection = !parent;
+
+  const { data: forestsData, loading: forestsLoading } = useQuery(QUERY_CHOOSE_FOREST, {
     variables: {
-      filter: {
-        _id: { eq: forest },
-      },
+      filter: forest ? { _id: { eq: forest } } : {},
     },
-    skip: !!parent,
+    skip: !hasForestSelection,
   });
 
   const prepareForestOptions = (data) => {
-    return data?.forests.edges.map(({ node }) =>({
+    const options = data.forests.edges.map(({ node }) =>({
       value: node._id,
       label: `${node.name} (${node.storiesCount} stories)`,
     }));
+
+    if (!options.length) {
+      options.push({
+        value: '',
+        label: 'No forest found...',
+      });
+    }
+
+    return options;
   };
 
   return (
     <div className={className}>
       <AuthRequired>
-        {!parent && forest && !forestsData &&
-          <Spinner loading={loadingForests} />
-        }
-        {(!!parent || (!parent && forest && forestsData)) &&
-          <Formik
-            initialValues={{
-              title: '',
-              content: '',
-              parent: parent?._id,
-              addTag: '',
-              tags: [],
-              forest: forestsData?.forests.edges.slice(0, 1).pop()?.node._id,
-              forests: prepareForestOptions(forestsData),
-              forestsLoading: false,
-            }}
-            validationSchema={Yup.object().shape({
-              title: Yup.string().required(true),
-              content: Yup.string().required(true),
-              forest: Yup.string().when('parent', {
-                is: () => !parent,
-                then: Yup.string().required(true),
-              })
-            })}
-            onSubmit={({ title, content, parent, tags, forest }, { resetForm }) => createStory({
-              variables: { input: { data: {
-                title, content, parent, forest, tags
-              }}},
-              onCompleted: (data) => {
-                !forest && resetForm();
-                gtag.event({
-                  action: 'create-story',
-                  category: 'story',
-                  label: 'success',
-                });
-                router.push(getStoryUrl(data.createStory.story));
-              },
-            })}
-          >
-            {({ isSubmitting, values, setFieldValue, errors, touched }) => (
-              <Form className="flex flex-col gap-sm">
-                <Field
-                  as={FormField}
-                  name="title"
-                  type="text"
-                  label="Title"
-                  error={errors.title}
-                  touched={touched.title}
-                />
-                <Field
-                  as={FormField}
-                  name="content"
-                  type="textarea"
-                  label="Content"
-                  rows="10"
-                  error={errors.content}
-                  touched={touched.content}
-                />
-                <FieldArray
-                  name="tags"
-                  render={arrayHelpers => (
-                    <div className="flex flex-col gap-sm">
-                      <div className="flex gap-sm justify-items-stretch items-center">
-                        <Field className="grow" name="addTag" as={FormField} type="text" />
-                        <Button type="button" size="md" onClick={() => { 
-                          if(values.tags.includes(values.addTag) || !values.addTag) {
-                            return;
-                          } 
-                          arrayHelpers.push(values.addTag);
-                          setFieldValue('addTag', '');
-                        }}>Add Tag</Button>
-                      </div>
-                      {!!values.tags.length && (
-                        <ul className="flex flex-wrap gap-xs">
-                          {values.tags.map((tag, index) => (
-                            <Button size="sm" key={index}>{tag} <FaTimes onClick={() => arrayHelpers.remove(index)}/></Button>
-                          ))}
-                        </ul>
-                      )}
-                    </div>
-                  )}
-                />
-
-                {!values.parent &&
+        <Formik
+          enableReinitialize
+          initialValues={{
+            title: '',
+            content: '',
+            parent: parent?._id,
+            addTag: '',
+            tags: [],
+            forest: forestsData ? prepareForestOptions(forestsData).pop() : '',
+            forests: forestsData ? prepareForestOptions(forestsData) : [],
+            forestsLoading: forestsLoading,
+            hasForestSelection,
+          }}
+          validationSchema={Yup.object().shape({
+            title: Yup.string().required(true),
+            content: Yup.string().required(true),
+            forest: Yup.string().when('parent', {
+              is: () => hasForestSelection,
+              then: Yup.string().required(true),
+            })
+          })}
+          onSubmit={({ title, content, parent, tags, forest }) => createStory({
+            variables: { input: { data: {
+              title, content, parent, forest, tags
+            }}},
+            onCompleted: (data) => {
+              gtag.event({
+                action: 'create-story',
+                category: 'story',
+                label: 'success',
+              });
+              router.push(getStoryUrl(data.createStory.story));
+            },
+          })}
+        >
+          {({ isSubmitting, values, setFieldValue, errors, touched }) => (
+            <Form className="flex flex-col gap-sm">
+              <Field
+                as={FormField}
+                name="title"
+                type="text"
+                label="Title"
+                error={errors.title}
+                touched={touched.title}
+              />
+              <Field
+                as={FormField}
+                name="content"
+                type="textarea"
+                label="Content"
+                rows="10"
+                error={errors.content}
+                touched={touched.content}
+              />
+              <FieldArray
+                name="tags"
+                render={arrayHelpers => (
                   <div className="flex flex-col gap-sm">
-                    <Text>Forest</Text>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-sm w-full"> 
-                      <Field
-                        as={FormField} 
-                        name="searchForest"
-                        className="grow"
-                        placeholder="Search..."
-                        type="text" 
-                        onChange={async (e) => {
-                          setFieldValue('forestsLoading', true);
-                          const { data } = await apolloClient.query({
-                            query: QUERY_CHOOSE_FOREST,
-                            variables: { filter: { query: e.target.value } },
-                          });
-                          setFieldValue('forestsLoading', false);
-                          setFieldValue('forests', prepareForestOptions(data));
-                        }} 
-                      />
-                      <Field 
-                        as={FormField}
-                        className={clsx(values.forestsLoading && 'animate-pulse')}
-                        name="forest"
-                        type="select"
-                        error={errors.forest}
-                        touched={touched.forest}
-                        options={values.forests}
-                      />
+                    <div className="flex gap-sm justify-items-stretch items-center">
+                      <Field className="grow" name="addTag" as={FormField} type="text" />
+                      <Button type="button" size="md" onClick={() => { 
+                        if(values.tags.includes(values.addTag) || !values.addTag) {
+                          return;
+                        } 
+                        arrayHelpers.push(values.addTag);
+                        setFieldValue('addTag', '');
+                      }}>Add Tag</Button>
                     </div>
+                    {!!values.tags.length && (
+                      <ul className="flex flex-wrap gap-xs">
+                        {values.tags.map((tag, index) => (
+                          <Button size="sm" key={index}>{tag} <FaTimes onClick={() => arrayHelpers.remove(index)}/></Button>
+                        ))}
+                      </ul>
+                    )}
                   </div>
-                }
+                )}
+              />
 
-                <ApiError error={error} />
-                <Button
-                  type="submit"
-                  disabled={isSubmitting}
-                  loading={isSubmitting}
-                  className="w-full mt-sm">
-                  Create
-                  </Button>
-              </Form>
-            )}
-          </Formik>
-        }
+              {values.hasForestSelection &&
+                <div className="flex flex-col gap-sm">
+                  <Text>Forest</Text>
+                  <div className="grid grid-cols-1 lg:grid-cols-2 gap-sm w-full"> 
+                    <Field
+                      as={FormField} 
+                      name="searchForest"
+                      className="grow"
+                      placeholder="Search..."
+                      type="text" 
+                      onChange={async (e) => {
+                        setFieldValue('forestsLoading', true);
+                        setFieldValue('forest', '');
+                        const { data } = await apolloClient.query({
+                          query: QUERY_CHOOSE_FOREST,
+                          variables: { filter: { query: e.target.value } },
+                        });
+                        setFieldValue('forests', prepareForestOptions(data));
+                        setFieldValue('forest', prepareForestOptions(data).pop()?.value);
+                        setFieldValue('forestsLoading', false);
+                      }} 
+                    />
+                    <Field 
+                      as={FormField}
+                      className={clsx(values.forestsLoading && 'animate-pulse')}
+                      name="forest"
+                      type="select"
+                      error={errors.forest}
+                      disabled={!values.forest}
+                      touched={touched.forest}
+                      options={values.forests}
+                    />
+                  </div>
+                </div>
+              }
+
+              <ApiError error={error} />
+              <Button
+                type="submit"
+                disabled={isSubmitting}
+                loading={isSubmitting}
+                className="w-full mt-sm">
+                Create
+                </Button>
+            </Form>
+          )}
+        </Formik>
       </AuthRequired>
     </div>
   );
