cc1cfd31b654d0e4a94d0153cdc56855493f645a
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-05-27 18:46:48 +0200

    improve cookie consent flashing

--- components/common/CookieConsent/index.js
+++ components/common/CookieConsent/index.js
@@ -1,31 +1,36 @@
 import clsx from 'clsx';
 import { Button, Text, Container } from 'components/ui';
-import { useState, useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 import * as gtag from 'lib/gtag';
+import { useState, useEffect } from 'react';
 
 const cookieName = 'cookie-consent';
 const CookieConsent = ({ className }) => {
-  const [accepted, setAccepted] = useState(false);
+  const [visible, setVisible] = useState(false);
   const [cookie, setCookie] = useCookies([cookieName]);
 
   useEffect(() => {
-    if (cookie[cookieName]) {
-      gtag.event({
-        action: 'accept-cookie-consent',
-        category: 'cookie-consent',
-      });
-      setAccepted(true);
+    if (!cookie[cookieName]) {
+      setVisible(true);
     }
   }, [cookie[cookieName]]);
 
-  return (!accepted &&
+  const acceptCookieConsent = () => {
+    gtag.event({
+      action: 'accept-cookie-consent',
+      category: 'cookie-consent',
+    });
+    setCookie(cookieName, true, { path: '/', maxAge: 365 * 24 * 60 * 60 });
+    setVisible(false);
+  };
+
+  return (visible &&
     <div className={clsx('fixed bottom-0 left-0 w-full p-md border-t-2 bg-base', className)}>
       <Container className="flex flex-row gap-md justify-center items-center">
         <Text>
           This site uses cookies to improve your experience.
         </Text>
-        <Button onClick={() => setCookie(cookieName, true, { path: '/', maxAge: 365 * 24 * 60 * 60 })}>ACCEPT</Button>
+        <Button onClick={() => acceptCookieConsent()}>ACCEPT</Button>
       </Container>
     </div>
   );
