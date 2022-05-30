e2bfacb5e899b9d92961fab602e9383dc69890e7
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-05-31 00:23:53 +0200

    add liked stories profile page

---
+++ pages/profile/liked-stories.js
@@ -0,0 +1,34 @@
+import { ProfileLayout } from 'components/layout';
+import { Text } from 'components/ui';
+import { PageIntro } from 'components/common';
+import { StoryList } from 'components/story';
+import Head from 'next/head';
+import { useState } from 'react';
+
+const LikedStories = () => {
+  const [storiesCount, setStoriesCount] = useState();
+
+  return (
+    <>
+      <PageIntro>
+        <Head>
+          <title>Liked stories - Treefingers</title>
+        </Head>
+        <div className="flex gap-sm justify-between items-center">
+          <Text variant="pageTitle">Stories you like</Text>
+        </div>
+        <Text variant="p">
+          Here is a list of the stories you've liked.
+        </Text>
+      </PageIntro>
+      {storiesCount === 0 &&
+        <Text>You haven't planted any story yet. What are you waiting for?</Text>
+      }
+      <StoryList className="grid md:grid-cols-2 gap-md" liked setTotalCount={setStoriesCount}/>
+    </>
+  );
+};
+
+LikedStories.Layout = ProfileLayout;
+
+export default LikedStories;
