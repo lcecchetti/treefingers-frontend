f8434d1be3b179501130b90077a923a641eeed8a
Luca Cecchetti <luca.cecchetti07@gmail.com>
2022-07-17 01:43:56 +0200

    limit max number of notifications count

--- components/user/CurrentUser/index.js
+++ components/user/CurrentUser/index.js
@@ -14,13 +14,13 @@ const CurrentUser = ({ className }) => {
     pollInterval: 60000,
     skip: !currentUser,
   });
-  
+
   return (
     <div className={clsx(className, 'relative')}>
       <FaUserCircle/>
       {data?.notifications && !!data.notifications.unreadCount &&
         <Text className="bg-red bg-full rounded-full text-primary-contrast absolute left-1/2 top-1/2 text-xs p-xs w-lg h-lg text-center">
-          {data.notifications.unreadCount}
+          {data.notifications.unreadCount > 99 ? '99+' : data.notifications.unreadCount}
         </Text>
       }
     </div>
